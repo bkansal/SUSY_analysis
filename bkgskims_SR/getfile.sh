f779cc5dfb2bd5d1896a37f61190f8de8e3bb59f
bhumika kansal <bhumika.kansal@cern.ch>
2020-01-06 01:19:40 -0600

    bkg skims for SR scripts

---
+++ bkgskims_SR/getfile.sh
@@ -0,0 +1,25 @@
+ls /eos/uscms/store/group/lpcsusyhad/SusyPhotonMET/Run2ProductionV17/background/Summer16v3.ZNuNuGJets_MonoPhoton_PtG-40to130_TuneCP5_13TeV-madgraph_*_RA2AnalysisTree.root > Summer16v3.ZNuNuGJets_MonoPhoton_PtG-40to130_TuneCP5_13TeV-madgraph.txt
+sed -i "s#/eos/uscms/store/#root://cmseos.fnal.gov//store/#" Summer16v3.ZNuNuGJets_MonoPhoton_PtG-40to130_TuneCP5_13TeV-madgraph.txt
+ls /eos/uscms/store/group/lpcsusyhad/SusyPhotonMET/Run2ProductionV17/background/Summer16v3.ZNuNuGJets_MonoPhoton_PtG-130_TuneCP5_13TeV-madgraph_*_RA2AnalysisTree.root > Summer16v3.ZNuNuGJets_MonoPhoton_PtG-130_TuneCP5_13TeV-madgraph.txt
+sed -i "s#/eos/uscms/store/#root://cmseos.fnal.gov//store/#" Summer16v3.ZNuNuGJets_MonoPhoton_PtG-130_TuneCP5_13TeV-madgraph.txt
+
+
+
+
+# ls /eos/uscms/store/group/lpcsusyhad/SusyPhotonMET/Run2ProductionV17/background/ZNuNuGJets_MonoPhoton_PtG-40to130_TuneCP5_13TeV-madgraph_*_RA2AnalysisTree.root > Fall17.ZNuNuGJets_MonoPhoton_PtG-40to130_TuneCP5_13TeV-madgraph.txt
+# sed -i "s#/eos/uscms/store/#root://cmseos.fnal.gov//store/#" Fall17.ZNuNuGJets_MonoPhoton_PtG-40to130_TuneCP5_13TeV-madgraph.txt
+# ls /eos/uscms/store/group/lpcsusyhad/SusyPhotonMET/Run2ProductionV17/background/ZNuNuGJets_MonoPhoton_PtG-130_TuneCP5_13TeV-madgraph_*_RA2AnalysisTree.root > Fall17.ZNuNuGJets_MonoPhoton_PtG-130_TuneCP5_13TeV-madgraph.txt
+# sed -i "s#/eos/uscms/store/#root://cmseos.fnal.gov//store/#" Fall17.ZNuNuGJets_MonoPhoton_PtG-130_TuneCP5_13TeV-madgraph.txt
+# ls /eos/uscms/store/group/lpcsusyhad/SusyPhotonMET/Run2ProductionV17/background/WGJets_MonoPhoton_PtG-40to130_TuneCP5_13TeV-madgraph_*_RA2AnalysisTree.root > Fall17.WGJets_MonoPhoton_PtG-40to130_TuneCP5_13TeV-madgraph.txt
+# sed -i "s#/eos/uscms/store/#root://cmseos.fnal.gov//store/#" Fall17.WGJets_MonoPhoton_PtG-40to130_TuneCP5_13TeV-madgraph.txt
+# ls /eos/uscms/store/group/lpcsusyhad/SusyPhotonMET/Run2ProductionV17/background/WGJets_MonoPhoton_PtG-130_TuneCP5_13TeV-madgraph_*_RA2AnalysisTree.root > Fall17.WGJets_MonoPhoton_PtG-130_TuneCP5_13TeV-madgraph.txt
+# sed -i "s#/eos/uscms/store/#root://cmseos.fnal.gov//store/#" Fall17.WGJets_MonoPhoton_PtG-130_TuneCP5_13TeV-madgraph.txt
+# # WGJets_MonoPhoton_PtG-130_TuneCP5_13TeV-madgraph_9_RA2AnalysisTree.root
+# ls /eos/uscms/store/group/lpcsusyhad/SusyPhotonMET/Run2ProductionV17/background/Autumn18.ZNuNuGJets_MonoPhoton_PtG-40to130_TuneCP5_13TeV-madgraph_*_RA2AnalysisTree.root > Autumn18.ZNuNuGJets_MonoPhoton_PtG-40to130_TuneCP5_13TeV-madgraph.txt
+# sed -i "s#/eos/uscms/store/#root://cmseos.fnal.gov//store/#" Autumn18.ZNuNuGJets_MonoPhoton_PtG-40to130_TuneCP5_13TeV-madgraph.txt
+# ls /eos/uscms/store/group/lpcsusyhad/SusyPhotonMET/Run2ProductionV17/background/Autumn18.ZNuNuGJets_MonoPhoton_PtG-130_TuneCP5_13TeV-madgraph_*_RA2AnalysisTree.root > Autumn18.ZNuNuGJets_MonoPhoton_PtG-130_TuneCP5_13TeV-madgraph.txt
+# sed -i "s#/eos/uscms/store/#root://cmseos.fnal.gov//store/#" Autumn18.ZNuNuGJets_MonoPhoton_PtG-130_TuneCP5_13TeV-madgraph.txt
+# ls /eos/uscms/store/group/lpcsusyhad/SusyPhotonMET/Run2ProductionV17/background/Autumn18.WGJets_MonoPhoton_PtG-40to130_TuneCP5_13TeV-madgraph_*_RA2AnalysisTree.root > Autumn18.WGJets_MonoPhoton_PtG-40to130_TuneCP5_13TeV-madgraph.txt
+# sed -i "s#/eos/uscms/store/#root://cmseos.fnal.gov//store/#" Autumn18.WGJets_MonoPhoton_PtG-40to130_TuneCP5_13TeV-madgraph.txt
+# ls /eos/uscms/store/group/lpcsusyhad/SusyPhotonMET/Run2ProductionV17/background/Autumn18.WGJets_MonoPhoton_PtG-130_TuneCP5_13TeV-madgraph_*_RA2AnalysisTree.root > Autumn18.WGJets_MonoPhoton_PtG-130_TuneCP5_13TeV-madgraph.txt
+# sed -i "s#/eos/uscms/store/#root://cmseos.fnal.gov//store/#" Autumn18.WGJets_MonoPhoton_PtG-130_TuneCP5_13TeV-madgraph.txt
